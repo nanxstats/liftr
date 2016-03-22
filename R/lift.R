8a0a50f44b6c4171f66c4bb7a854912c37a439b2
Tengfei Yin <tengfei.yin@sbgenomics.com>
2016-03-22 18:37:54 -0400

    add lift docopt and shiny apps and cmd

--- R/lift.R
+++ R/lift.R
@@ -10,22 +10,23 @@
 #' See \code{vignette('liftr-intro')} for details about the extended
 #' YAML front-matter metadata format used by liftr.
 #'
-#' @param input Input (R Markdown) file.
+#' @param input Input (R Markdown) file or shiny app folder.
 #' @param output_dir Directory to output \code{Dockerfile}.
 #' If not provided, will be the same directory as \code{input}.
+#' @param ... Extra arguments passed to liftShinyApp function.
 #'
 #' @return \code{Dockerfile} (and \code{Rabixfile} if possible).
 #'
 #' @export lift
 #'
 #' @importFrom knitr knit
-#' @importFrom yaml yaml.load
+#' @importFrom yaml yaml.load as.yaml
 #'
 #' @examples
 #' # 1. Dockerized R Markdown document
 #' dir_docker = paste0(tempdir(), '/lift_docker/')
 #' dir.create(dir_docker)
-#' file.copy(system.file("docker.Rmd", package = "liftr"), dir_docker)
+#' file.copy(system.file("examples/docker.Rmd", package = "liftr"), dir_docker)
 #' # use lift() to parse Rmd and generate Dockerfile
 #' lift(paste0(dir_docker, "docker.Rmd"))
 #' # view generated Dockerfile
@@ -34,40 +35,32 @@
 #' # 2. Dockerized R Markdown document with Rabix options
 #' dir_rabix = paste0(tempdir(), '/lift_rabix/')
 #' dir.create(dir_rabix)
-#' file.copy(system.file("rabix.Rmd", package = "liftr"), dir_rabix)
+#' file.copy(system.file("template/rabix.Rmd", package = "liftr"), dir_rabix)
 #' lift(input = paste0(dir_rabix, "rabix.Rmd"))
 #' # view generated Dockerfile
 #' readLines(paste0(dir_rabix, "Dockerfile"))
 #' # view generated Rabixfile
 #' readLines(paste0(dir_rabix, "Rabixfile"))
-lift = function(input = NULL, output_dir = NULL) {
+lift = function(input = NULL, output_dir = NULL, ...) {
 
   if (is.null(input))
     stop('missing input file')
-  if (!file.exists(normalizePath(input)))
-    stop('input file does not exist')
-
-  # locate YAML metadata block
-  doc_content = readLines(normalizePath(input))
-  header_pos = which(doc_content == '---')
-
-  # handling YAML blocks ending with three dots
-  if (length(header_pos) == 1L) {
-    header_dot_pos = which(doc_content == '...')
-    if (length(header_dot_pos) == 0L) {
-      stop('Cannot correctly locate YAML metadata block.
-           Please use three hyphens (---) as start line & end line,
-           or three hyphens (---) as start line with three dots (...)
-           as end line.')
-    } else {
-      header_pos[2L] = header_dot_pos[1L]
+  if(file.info(input)$isdir){
+    message("input is folder, treat it as shiny app folder")
+    ## treat as shiny app folder
+    message("parsing dependecies and genrate liftr file ...")
+    lift_shinyapp(input, output_dir = output_dir, ...)
+    return()
+  }else{
+    ## treat as file
+    if (!file.exists(normalizePath(input))){
+      stop('input file or shiny app folder does not exist')
     }
   }
 
-  doc_yaml = paste(doc_content[(header_pos[1L] + 1L):
-                                 (header_pos[2L] - 1L)],
-                   collapse = '\n')
-  opt_all_list = yaml.load(doc_yaml)
+
+
+  opt_all_list = parse_rmd(input)
 
   # liftr options handling
   if (is.null(opt_all_list$liftr))
@@ -98,7 +91,7 @@ lift = function(input = NULL, output_dir = NULL) {
   # system dependencies
   if (!is.null(opt_list$syslib)) {
     liftr_syslib =
-      paste(readLines(system.file('syslib.Rmd', package = 'liftr')),
+      paste(readLines(system.file('template/syslib.Rmd', package = 'liftr')),
             paste(opt_list$syslib, collapse = ' '), sep = ' ')
   } else {
     liftr_syslib = NULL
@@ -108,7 +101,7 @@ lift = function(input = NULL, output_dir = NULL) {
   if (!is.null(opt_list$latex)) {
     if (opt_list$latex == TRUE) {
       liftr_texlive =
-        paste(readLines(system.file('texlive.Rmd', package = 'liftr')),
+        paste(readLines(system.file('template/texlive.Rmd', package = 'liftr')),
               collapse = '\n')
     } else {
       liftr_texlive = NULL
@@ -127,11 +120,11 @@ lift = function(input = NULL, output_dir = NULL) {
         liftr_pandoc = NULL
       } else {
         liftr_pandoc = paste(readLines(
-          system.file('pandoc.Rmd', package = 'liftr')), collapse = '\n')
+          system.file('template/pandoc.Rmd', package = 'liftr')), collapse = '\n')
       }
     } else {
       liftr_pandoc = paste(readLines(
-        system.file('pandoc.Rmd', package = 'liftr')), collapse = '\n')
+        system.file('template/pandoc.Rmd', package = 'liftr')), collapse = '\n')
     }
   }
 
@@ -143,7 +136,7 @@ lift = function(input = NULL, output_dir = NULL) {
   if (!is.null(opt_list$cranpkg)) {
     liftr_cranpkgs = quote_str(opt_list$cranpkg)
     tmp = tempfile()
-    invisible(knit(input = system.file('cranpkg.Rmd', package = 'liftr'),
+    invisible(knit(input = system.file('template/cranpkg.Rmd', package = 'liftr'),
                    output = tmp, quiet = TRUE))
     liftr_cranpkg = readLines(tmp)
   } else {
@@ -154,7 +147,7 @@ lift = function(input = NULL, output_dir = NULL) {
   if (!is.null(opt_list$biocpkg)) {
     liftr_biocpkgs = quote_str(opt_list$biocpkg)
     tmp = tempfile()
-    invisible(knit(input = system.file('biocpkg.Rmd', package = 'liftr'),
+    invisible(knit(input = system.file('template/biocpkg.Rmd', package = 'liftr'),
                    output = tmp, quiet = TRUE))
     liftr_biocpkg = readLines(tmp)
   } else {
@@ -165,7 +158,7 @@ lift = function(input = NULL, output_dir = NULL) {
   if (!is.null(opt_list$ghpkg)) {
     liftr_ghpkgs = quote_str(opt_list$ghpkg)
     tmp = tempfile()
-    invisible(knit(input = system.file('ghpkg.Rmd', package = 'liftr'),
+    invisible(knit(input = system.file('template/ghpkg.Rmd', package = 'liftr'),
                    output = tmp,
                    quiet = TRUE))
     liftr_ghpkg = readLines(tmp)
@@ -173,10 +166,17 @@ lift = function(input = NULL, output_dir = NULL) {
     liftr_ghpkg = NULL
   }
 
+  # extra: plain docker file line, like ADD, COPY, CMD etc
+  if (!is.null(opt_list$extra)) {
+    liftr_extra = opt_list$extra
+  } else {
+    liftr_extra = NULL
+  }
+
   # write Dockerfile
   if (is.null(output_dir)) output_dir = file_dir(input)
 
-  invisible(knit(system.file('Dockerfile.Rmd',
+  invisible(knit(system.file('template/Dockerfile.Rmd',
                              package = 'liftr'),
                  output = paste0(normalizePath(output_dir),
                                  '/Dockerfile'),
@@ -210,7 +210,7 @@ lift = function(input = NULL, output_dir = NULL) {
         liftr_rabix_args = NULL
       }
 
-      invisible(knit(system.file('Rabixfile.Rmd',
+      invisible(knit(system.file('template/Rabixfile.Rmd',
                                  package = 'liftr'),
                      output = paste0(normalizePath(output_dir),
                                      '/Rabixfile'),
@@ -220,3 +220,299 @@ lift = function(input = NULL, output_dir = NULL) {
   }
 
 }
+
+#' parse Rmarkdown header
+#'
+#' parse Rmarkdown header and return a list
+#'
+#' The header section is use three hyphens --- as start line & end line,
+#' or three hyphens --- as start line with three dots ...
+#' as end line
+#'
+#' @export parse_rmd
+#' @aliases parse_rmd
+#' @examples
+#' fl = system.file("examples/docker.Rmd", package = "liftr")
+#' parse_rmd(fl)
+parse_rmd = function(input){
+  # locate YAML metadata block
+  doc_content = readLines(normalizePath(input))
+  header_pos = which(doc_content == '---')
+
+  # handling YAML blocks ending with three dots
+  if (length(header_pos) == 1L) {
+    header_dot_pos = which(doc_content == '...')
+    if (length(header_dot_pos) == 0L) {
+      stop('Cannot correctly locate YAML metadata block.
+           Please use three hyphens (---) as start line & end line,
+           or three hyphens (---) as start line with three dots (...)
+           as end line.')
+    } else {
+      header_pos[2L] = header_dot_pos[1L]
+    }
+  }
+
+  doc_yaml = paste(doc_content[(header_pos[1L] + 1L):
+                                 (header_pos[2L] - 1L)],
+                   collapse = '\n')
+  yaml.load(doc_yaml)
+}
+
+
+trans_name <- function(x){
+  names(x)[names(x) == "Bioconductor"] <- "biocpkg"
+  names(x)[names(x) == "CRAN"] <- "cranpkg"
+  x
+}
+
+
+create_lift_file = function(appDir = getwd(), appFiles = NULL, output_file = "docker.Rmd",
+                            maintainer = NULL, email = NULL){
+  stopifnot(dir.exists(appDir))
+  .out <- file.path(normalizePath(appDir), output_file)
+  ## add dummy maintain name
+  if(is.null(maintainer)){
+    maintainer = Sys.info()[names(Sys.info()) == "user"]
+    message("maintainer name is not provided, user your system user name as maintainer name: ", maintainer)
+  }
+  if(is.null(email)){
+    email = paste0(maintainer, "@dummy.com")
+    message("email is not provided, create fake email address for placeholder: ", email)
+  }
+  .h <- list(maintainer = maintainer,
+             maintainer_email = email)
+  ## add dummy email name
+  ## search for liftr.rmd
+  if(!file.exists(.out)){
+    ad = appDependencies(appDir = appDir, appFiles = appFiles)
+    lst = by(ad, ad$source, function(x){
+      as.list(x$package)
+    })
+    lst = trans_name(lst)
+    res = list(liftr = c(.h,lst))
+    message("Ouput file: ", .out)
+    con = file(.out)
+    txt = "---"
+    txt = c(txt, as.yaml(res))
+    txt = c(txt, "---")
+    writeLines(txt, con = .out)
+    close(con)
+  }else{
+    message(.out, " exists.")
+  }
+  .out
+}
+
+#' Dockerize an Shiny App
+#'
+#' Parse dependecies from a shiny app folder and lift it into a Dockerfile
+#'
+#' @param appDir Directory containing application. Defaults to current working directory.
+#' @param appFiles The files and directories to bundle and deploy (only if upload = TRUE). Can be NULL, in which case all the files in the directory containing the application are bundled. Takes precedence over appFileManifest if both are supplied.
+#' @param output_file A temporariy R markdown file with liftr header passed from shina app folder.
+#' @param output_dir output_dir Directory to output \code{Dockerfile}. If not provided, will be the same directory as \code{input}.
+#' @export lift_shinyapp
+#' @aliases lift_shinyapp
+#' @examples
+#' \dontrun{
+#' lift_shinayapp("test_app_folder")
+#' }
+lift_shinyapp <- function(appDir = getwd(), appFiles = NULL, output_file = "docker.Rmd", output_dir = NULL,
+                          maintainer = NULL, email = NULL){
+  .out <- create_lift_file(appDir = appDir, appFiles = appFiles, output_file = output_file,
+                           maintainer = maintainer, email = email)
+  lift(.out, output_dir)
+}
+
+
+
+#' lift a docopt string
+#'
+#' lift a docopt string used for command line
+#'
+#' parse Rmarkdown header from rabix field
+#'
+#' @param input input Rmarkdown file or a function name (character)
+#' @export lift_docopt
+#' @aliases lift_docopt
+#' @return a string used for docopt
+#' @examples
+#' fl = system.file("examples/runif.Rmd", package = "liftr")
+#' opts = lift_docopt(fl)
+#' \dontrun{
+#' require(docopt)
+#' docopt(opts)
+#' docopt(lift_docopt("mean.default"))
+#' }
+lift_docopt = function(input){
+
+  if(file.exists(input)){
+    res = lift_docopt_from_header(input)
+  }else{
+    message("file doesn't exist, try to try this as a function")
+    res = lift_docopt_from_function(input)
+  }
+  res
+}
+
+
+lift_docopt_from_header = function(input){
+  opt_all_list = parse_rmd(input)
+  ol <- opt_all_list$rabix
+  .in <- ol$inputs
+  txt <- paste("usage:", ol$baseCommand, "[options]")
+  txt <- c(txt, "options:")
+
+  ol <- lapply(.in, function(x){
+    .nm <- x$prefix
+    .t <- x$type
+    .type <- paste0('<', deType(.t), '>')
+    .o <- paste(.nm, .type, sep = "=")
+    .des <- x$description
+    .default <- x$default
+    if(!is.null(.default)){
+      .des <- paste0(.des, " [default: ", .default, "]")
+    }
+    list(name = .o, description = .des)
+  })
+  for(i in 1:length(ol)){
+    txt <- c(txt, paste(" ", ol[[i]]$name, ol[[i]]$description))
+  }
+  paste(txt, collapse = "\n")
+}
+
+lift_docopt_from_function = function(input){
+
+  ol = opt_all_list = rdarg(input)
+
+  txt <- paste0("usage: ", input, ".R",  " [options]")
+
+
+  nms <- names(ol)
+  lst <- NULL
+
+  for(nm in nms){
+    .nm = paste0("--", nm)
+    .t = guess_type(nm, input)
+    .type = paste0('<', deType(.t), '>')
+    .o = paste(.nm, .type, sep = "=")
+    .des = ol[[nm]]
+    .def  = guess_default(nm, input)
+    if(!is.null(.def)){
+      .des <- paste0(.des, " [default: ", .def, "]")
+    }
+    lst = c(lst, list(list(name = .o, description = .des)))
+  }
+
+  for(i in 1:length(lst)){
+    txt <- c(txt, paste(" ", lst[[i]]$name, lst[[i]]$description))
+  }
+  ## Fixme:
+  paste(txt, collapse = "\n")
+}
+
+
+lift_cmd = function(input, output_dir = NULL, shebang = "#!/usr/local/bin/Rscript",
+                    docker_root = "/"){
+
+  if(file.exists(input)){
+    opt_all_list = parse_rmd(input)
+    if (is.null(output_dir))
+      output_dir = dirname(normalizePath(input))
+
+    tmp = file.path(output_dir, opt_all_list$rabix$baseCommand)
+    message("command line file: ", tmp)
+    con = file(tmp)
+    txt = lift_docopt(input)
+    txt = c(shebang, "'", paste0(txt, " ' -> doc"))
+    paste("library(docopt)\n opts <- docopt(doc) \n
+        rmarkdown::render('",
+          docker_root, basename(input), "', BiocStyle::html_document(toc = TRUE),
+        output_dir = '.', params = lst)
+    " )-> .final
+    txt <- c(txt, .final)
+    writeLines(txt, con = con)
+    close(con)
+  }else{
+    message("consider you passed a function name (character)")
+    if (is.null(output_dir))
+      output_dir = getwd()
+    .baseCommand <- paste0(input, ".R")
+    tmp = file.path(output_dir, .baseCommand)
+    message("command line file: ", tmp)
+    con = file(tmp)
+    txt = lift_docopt(input)
+    txt = c(shebang, "'", paste0(txt, " ' -> doc"))
+    txt = c(txt, "library(docopt)\n opts <- docopt(doc)")
+    .final = gen_list(input)
+    txt <- c(txt, .final)
+    writeLines(txt, con = con)
+    close(con)
+  }
+  Sys.chmod(tmp)
+  tmp
+}
+
+con_fun = function(type){
+  res = switch(deType(type),
+          int = "as.integer",
+          float = "as.numeric",
+          boolean = "as.logical",
+          NULL)
+  res
+}
+
+
+gen_list = function(fun){
+  lst = rdarg(fun)
+  lst = lst[names(lst) != "..."]
+  nms = names(lst)
+  txt = NULL
+  for(nm in nms){
+    .t = con_fun(guess_type(nm, fun))
+    if(!is.null(.t)){
+      txt = c(txt, paste0(nm, " = ", .t, "(", "opts$", nm, ")"))
+    }else{
+      txt = c(txt, paste0(nm, " = ", "opts$", nm))
+    }
+
+  }
+  txt = paste("list(", paste(txt, collapse = ","), ")")
+  paste("do.call(", fun, ",", txt, ")")
+
+}
+
+
+guess_type = function(nm, fun){
+  dl = formals(fun)
+  if(!is.null(dl[[nm]])){
+    .c <- class(dl[[nm]])
+    if(.c == "name"){
+      return("string")
+    }else{
+      return(deType(.c))
+    }
+
+  }else{
+    return("string")
+  }
+}
+
+guess_default = function(nm, fun){
+  dl = formals(fun)
+  if(!is.null(dl[[nm]])){
+    .c <- class(dl[[nm]])
+    if(.c == "name"){
+      return(NULL)
+    }else{
+      return(dl[[nm]])
+    }
+
+  }else{
+    return(NULL)
+  }
+}
+
+lift_cmd("runif", file.path(getwd()))
+
+
--- R/utils.R
+++ R/utils.R
@@ -31,3 +31,137 @@ is_from_bioc = function (x) substr(str_trim(x), 1L, 13L) == 'bioconductor/'
 #' @importFrom stringr str_trim
 #' @noRd
 is_from_rstudio = function (x) substr(str_trim(x), 1L, 14L) == 'rocker/rstudio'
+
+
+deType <- function(x){
+
+  ## string
+  str_type <- c('STRING', 'STR', '<string>', '<str>', 'str', "character",
+                "string", "String")
+  ## int
+  int_type <- c('INTEGER', 'INT', '<integer>', '<int>', 'int',
+                "integer", "Integer")
+  ## float
+  float_type <- c('FLOAT', '<float>', 'float', 'Float', 'numeric')
+  ## File
+  file_type <- c('FILE', '<file>', 'File', 'file')
+
+  ## enum
+  enum_type <- c('ENUM', '<enum>', 'enum', "Enum")
+
+  ## boolean
+  boolean_type <- c('BOOLEAN', '<boolean>', 'boolean', "Boolean", "logical", "logic", "Logical")
+
+  .array <- FALSE
+  if(is.character(x)){
+    res <- ""
+    if(grepl("\\.\\.\\.", x)){
+      .array <- TRUE
+      x <- gsub("[^[:alnum:]]", "", x)
+    }
+
+    if(x %in% str_type){
+      res <- "string"
+    }else if(x %in% int_type){
+      res <- "int"
+    }else if(x %in% float_type){
+      res <- "float"
+    }else if(x %in% file_type){
+      res <- "File"
+    }else if(x %in% enum_type){
+      res <- "enum"
+    }else if(x %in% boolean_type){
+      res <- "boolean"
+    }else{
+      res <- x
+    }
+    if(.array){
+      res <- ItemArray(res)
+    }
+  }else{
+    res <- x
+  }
+  res
+}
+
+## copied from roxygen2
+rdarg <- function(topic, dots = FALSE){
+
+
+  internal_f <- function(p, f) {
+    stopifnot(is.character(p), length(p) == 1)
+    stopifnot(is.character(f), length(f) == 1)
+
+    get(f, envir = asNamespace(p))
+  }
+
+  get_rd <- function(topic, package = NULL) {
+    help_call <- substitute(help(t, p), list(t = topic, p = package))
+    top <- eval(help_call)
+    if (length(top) == 0) return(NULL)
+
+    internal_f("utils", ".getHelpFile")(top)
+  }
+
+  # get_rd should parse Rd into a rd_file so I don't need to maintain
+  # two parallel apis
+
+  get_tags <- function(rd, tag) {
+    rd_tag <- function(x) attr(x, "Rd_tag")
+
+    Filter(function(x) rd_tag(x) == tag, rd)
+  }
+
+  rd2rd <- function(x) {
+    chr <- internal_f("tools", "as.character.Rd")(x)
+    paste(unlist(chr), collapse = "")
+  }
+
+  # rd_arguments(get_rd("mean"))
+  rd_arguments <- function(rd) {
+    arguments <- get_tags(rd, "\\arguments")[[1]]
+    items <- get_tags(arguments, "\\item")
+
+    values <- lapply(items, function(x) rd2rd(x[[2]]))
+    params <- vapply(items, function(x) rd2rd(x[[1]]), character(1))
+
+    setNames(values, params)
+  }
+
+  res = rd_arguments(get_rd(topic))
+  if(!dots){
+    res = res[names(res) != "\\dots"]
+  }
+
+  res = sapply(res,
+         function(x){
+           x = gsub("\n" , "", x)
+           x = gsub("\\\\", "", x)
+         })
+
+  nms = names(formals(topic))
+  nms = setdiff(nms, "...")
+
+  res = split_arg(res)
+  res[names(res) %in% nms]
+}
+
+split_arg = function(x){
+  .arg = c()
+  for(nm in names(x)){
+
+    if(grepl(",", nm)){
+      nms = strsplit(nm, split = ",")[[1]]
+      nms = gsub("^\\s+|\\s+$", "", nms)
+      res = rep(x[nm], length(nms))
+      names(res) = nms
+    }else{
+      res = x[nm]
+    }
+   .arg = c(.arg, res)
+  }
+  .arg
+}
+
+
+
