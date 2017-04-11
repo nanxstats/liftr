6d8b220929b3f88b82aad6eaf7d57138d6cfe29d
Nan Xiao <road2stat@gmail.com>
2017-04-11 02:37:42 -0400

    add rstudio addins

---
+++ R/addins.R
@@ -0,0 +1,48 @@
+#' RStudio Addin: Containerize R Markdown Document
+#'
+#' Call this function as an addin to dockerize (lift) the current document.
+#'
+#' @importFrom rstudioapi getActiveDocumentContext
+#'
+#' @export
+addin_lift = function() {
+
+  context = rstudioapi::getActiveDocumentContext()
+  path = normalizePath(context$'path')
+  lift(path)
+
+}
+
+#' RStudio Addin: Containerize and Render R Markdown Document with Docker
+#'
+#' Call this function as an addin to containerize and render
+#' the current document.
+#'
+#' @importFrom rstudioapi getActiveDocumentContext
+#'
+#' @export
+addin_lift_render_docker = function() {
+
+  context = rstudioapi::getActiveDocumentContext()
+  path = normalizePath(context$'path')
+  lift(path)
+  render_docker(path)
+
+}
+
+#' RStudio Addin: Purge Docker Image
+#'
+#' Call this function as an addin to removes the Docker image used
+#' for rendering the current document.
+#'
+#' @importFrom rstudioapi getActiveDocumentContext
+#'
+#' @export
+addin_purge_image = function() {
+
+  context = rstudioapi::getActiveDocumentContext()
+  path = normalizePath(context$'path')
+  path = paste0(file_dir(path), '/', file_name_sans(path), '.docker.yml')
+  purge_image(path)
+
+}
