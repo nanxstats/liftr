618f11c4e26fb6b150a0972af4512ab674aebc59
Nan Xiao <road2stat@gmail.com>
2019-06-18 15:24:04 -0400

    use simple package docs page declaration in roxygen2

--- R/liftr-package.R
+++ R/liftr-package.R
@@ -1,18 +1,2 @@
-#' Containerize R Markdown Documents for Continuous Reproducibility
-#'
-#' Persistent reproducible reporting by
-#' containerization of R Markdown documents.
-#'
-#' \tabular{ll}{
-#' Package: \tab liftr\cr
-#' Type: \tab Package\cr
-#' License: \tab GPL-3}
-#'
-#' @name liftr-package
-#' @docType package
-#' @author
-#' Nan Xiao <\email{me@@nanx.me}>
-#' Miao-Zhu Li <\email{miaozhu.li@@duke.edu}>
-#' Teng-Fei Yin <\email{tengfei.yin@@sevenbridges.com}>
-#'
-NULL
+#' @keywords internal
+"_PACKAGE"
